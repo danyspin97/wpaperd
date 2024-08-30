cc44aa9043d89e1ae059564009676da3e1c252be
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2024-08-30 21:05:23 +0200

    fix: type

--- daemon/src/render/shader.rs
+++ daemon/src/render/shader.rs
@@ -12,7 +12,7 @@ use super::gl;
 pub unsafe fn create_shader(
     gl: &gl::Gl,
     shader: gl::types::GLenum,
-    sources: &[*const i8],
+    sources: &[*const u8],
 ) -> Result<gl::types::GLuint> {
     let shader = gl.CreateShader(shader);
     gl_check!(gl, "calling CreateShader");
